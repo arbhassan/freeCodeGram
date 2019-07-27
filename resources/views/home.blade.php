02180197e525de85a3d3e59567ecbb01ce2087bc
arbhassan <hassanjawad1321@gmail.com>
2019-07-27 05:38:43 +0500

    Designing the UI from Instagram

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,36 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://z-p42-instagram.fisb3-1.fna.fbcdn.net/vp/b2bccbdfad9a37b5d828e03d1118c429/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ad=z-m&_nc_ht=z-p42-instagram.fisb3-1.fna.fbcdn.net" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="https://freecodecamp.org">www.freecodecamp.org</a></div>
+
+
         </div>
     </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://z-p42-instagram.fisb3-1.fna.fbcdn.net/vp/235fc9e5d3d68df3d9e96993e6325ad9/5DDED7A7/t51.2885-15/e35/c50.0.551.551/s320x320/66375760_146405949787171_8975628055082579520_n.jpg?_nc_ad=z-m&_nc_ht=z-p42-instagram.fisb3-1.fna.fbcdn.net" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://z-p42-instagram.fisb3-1.fna.fbcdn.net/vp/4842821f41c1c323019d74ba297414aa/5DE61B68/t51.2885-15/e35/s480x480/67720263_461097851109319_2975787659867457306_n.jpg?_nc_ad=z-m&_nc_ht=z-p42-instagram.fisb3-1.fna.fbcdn.net" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://z-p42-instagram.fisb3-1.fna.fbcdn.net/vp/3b3cc5180203330a8adcbd26099f794e/5DB26A13/t51.2885-15/e35/c0.54.847.847a/s480x480/66036540_355410028689526_4219555080293749431_n.jpg?_nc_ad=z-m&_nc_ht=z-p42-instagram.fisb3-1.fna.fbcdn.net" class="w-100" alt="">
+        </div>
+    </div>
+
 </div>
 @endsection
